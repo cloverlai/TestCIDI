c646ecc75de4cb2e3582a383347d06354cf53439
Thao Lai <thao.lai.clover@gmail.com>
2021-10-16 02:29:59 +0700

    changed swift version

--- Config/Config.swift
+++ Config/Config.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Thao Lai on 16/10/2021.
 //
-
+// test changed 
 import Foundation
 func infoForKey(_ key: String) -> String? {
         return (Bundle.main.infoDictionary?[key] as? String)?
